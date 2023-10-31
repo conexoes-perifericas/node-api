acda2d4eabd48e2389a607c0c29ab8dabaa3bdf5
Gildo Neto <gildoneto@hotmail.com>
2023-10-31 14:13:13 -0300

    atualiza create table

--- create-table.js
+++ create-table.js
@@ -1,12 +1,22 @@
 import sql from './db.js';
 
-sql`
-CREATE TABLE videos (
+// sql`DROP TABLE IF EXISTS videos`
+//   .then(() => {
+//     console.log('Tabela videos apagada')
+//   })
+//   .then(() => {
+//     process.exit(1);
+//   })
+
+sql`CREATE TABLE videos (
+  id          TEXT PRIMARY KEY,
   title       TEXT,
   description TEXT,
   duration    INTEGER
-);
-`
-.then(() => {
-  console.log('Tabela criada')
-})
+);`
+  .then(() => {
+    console.log('Tabela criada')
+  })
+  .then(() => {
+    process.exit(1);
+  })
